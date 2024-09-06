4630b65dc99105502d2e875ae901379a1804c389
gius <giuseppe.tortorella@dedoai.org>
2024-09-06 10:30:18 +0200

    add: db password secret manager

--- app/db.js
+++ app/db.js
@@ -1,10 +1,11 @@
 const { Pool } = require('pg');
+const { getDbSecretPwd } = require('./utils')
 
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
+    password: getDbSecretPwd(),
     port: process.env.DB_PORT
 });
 
--- app/utils.js
+++ app/utils.js
@@ -1,3 +1,11 @@
+const {
+    SecretsManagerClient,
+    GetSecretValueCommand,
+} = require('@aws-sdk/client-secrets-manager')
+
+const client = new SecretsManagerClient();
+const secret_name = "rds!db-328bc97f-8f6d-4cb8-a9e2-1ca01c0e88d1";
+
 const responseDTO = (statusCode, data) => {
     return {
         statusCode,
@@ -5,6 +13,19 @@ const responseDTO = (statusCode, data) => {
     }
 }
 
+const getDbSecretPwd = async () => {
+    const response = await client.send(new GetSecretValueCommand({ SecretId: process.env.DB_SECRET_PASS_ID }));
+
+    if (!response?.SecretString)
+        throw new Error('Failed to get secret')
+
+    return response?.SecretString;
+}
+
+
+const secret = response.SecretString;
+
 module.exports = {
-    responseDTO
-}   
+    responseDTO,
+    getDbSecretPwd
+}
