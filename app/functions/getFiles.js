b102e3da81e6d6088bc1fe7c268cd377e2aee2b0
gius <giuseppe.tortorella@dedoai.org>
2024-09-11 12:23:03 +0200

    list on get files

--- app/functions/getFiles.js
+++ app/functions/getFiles.js
@@ -7,7 +7,7 @@ const getFiles = async (getDTO) => {
     const db = await getDbConnection()
     const query = 'SELECT file_name,file_type,bucket_url FROM files WHERE entity_id = $1 and entity_name = $2 OFFSET $3 LIMIT $4'
     const result = await db.query(query, [id, 'c4d', limit, , offset])
-    return result.rows.length === 1 ? result.rows[0] : result.rows
+    return result.rows
 }
 
 module.exports = {
