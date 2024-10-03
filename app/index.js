d2ffe69c10af3c1c02a116c7ae5acd3839037bdb
gius <giuseppe.tortorella@dedoai.org>
2024-10-03 13:37:39 +0200

    add: public path

--- app/index.js
+++ app/index.js
@@ -13,30 +13,35 @@ exports.handler = async (event) => {
     try {
         let result;
         let validatedInput;
-        const { httpMethod, authorizer } = event.requestContext
-        if (!authorizer?.principalId) {
-            throw new ApplicationError('Unauthorized', 401)
-        }
+        const { httpMethod, authorizer, resourcePath } = event.requestContext
+        const isPublicRoute = resourcePath.endWith('public')
+        if (!isPublicRoute)
+            if (!authorizer?.principalId) {
+                throw new ApplicationError('Unauthorized', 401)
+            }
+        if (isPublicRoute)
+            event.queryStringParameters = { ...event.queryStringParameters, full: true }
+
         switch (httpMethod) {
             case 'GET':
-                console.log(`get request received with params: `, JSON.stringify({ ...event.queryStringParameters, user_id: authorizer.principalId }))
-                validatedInput = validate({ ...event.queryStringParameters, user_id: authorizer.principalId }, getDTO)
+                console.log(`get request received with params: `, JSON.stringify({ ...event.queryStringParameters, user_id: authorizer?.principalId }))
+                validatedInput = validate({ ...event.queryStringParameters, user_id: authorizer?.principalId }, getDTO)
                 result = await get(validatedInput)
                 return responseDTO(200, result)
             case 'POST':
-                console.log(`create request received with params: `, JSON.stringify({ ...event.body, user_id: authorizer.principalId }))
-                validatedInput = validate({ ...event.body, user_id: authorizer.principalId }, createDTO)
+                console.log(`create request received with params: `, JSON.stringify({ ...event.body, user_id: authorizer?.principalId }))
+                validatedInput = validate({ ...event.body, user_id: authorizer?.principalId }, createDTO)
                 result = await create(validatedInput)
                 return responseDTO(200, result)
             case 'PUT':
-                console.log(`update request received with params: `, JSON.stringify({ ...event.body, user_id: authorizer.principalId }))
-                validatedInput = validate({ ...event.body, user_id: authorizer.principalId }, updateDTO)
+                console.log(`update request received with params: `, JSON.stringify({ ...event.body, user_id: authorizer?.principalId }))
+                validatedInput = validate({ ...event.body, user_id: authorizer?.principalId }, updateDTO)
                 result = await update(validatedInput)
                 return responseDTO(200, result)
             case 'DELETE':
-                console.log(`delete request received with params: `, JSON.stringify({ ...event.queryStringParameters, user_id: authorizer.principalId }))
-                validatedInput = validate({ ...event.queryStringParameters, user_id: authorizer.principalId }, removeDTO)
-                result = await remove({ ...event.queryStringParameters, user_id: authorizer.principalId })
+                console.log(`delete request received with params: `, JSON.stringify({ ...event.queryStringParameters, user_id: authorizer?.principalId }))
+                validatedInput = validate({ ...event.queryStringParameters, user_id: authorizer?.principalId }, removeDTO)
+                result = await remove({ ...event.queryStringParameters, user_id: authorizer?.principalId })
                 return responseDTO(200, result)
             default:
                 return responseDTO(405, 'Method not allowed')
